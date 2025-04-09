51efb0243a9f82062e44386e02136ae0a1dc0f42
judellee212 <judellee@yahoo.ca>
2025-04-09 15:22:46 +0200

    Adding increased functionality
    Added select id and city timezones in html.
    Added function updateCity with timezone and cityTime.
    Replace citiesElement innerHTML with html coding for moment date and time

--- script/index.js
+++ script/index.js
@@ -1,12 +1,13 @@
 function updateTime() {
 let losAngelesElement = document.querySelector("#los-angeles");
+if (losAngelesElement) {
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
 let losAngelesTime = moment().tz("America/Los_Angeles");
 losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
 losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
-
+}
 let parisElement = document.querySelector("#paris");
 let parisDateElement = parisElement.querySelector(".date");
 let parisTimeElement = parisElement.querySelector(".time");
@@ -23,11 +24,30 @@ let hongKongTimeElement = hongKongElement.querySelector(".time");
 let hongKongTime = moment().tz("HongKong");
 hongKongDateElement.innerHTML = moment().format("MMM Do YYYY");
 hongKongTimeElement.innerHTML = hongKongTime.format("h:mm:ss [<small]>A[</small>]");
-
-
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value; 
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class = "city" 
+  <div>
+    <h2>${cityTimeZone}</h2>
+    <div class="date">${cityTime.format("MMM Do YYYY")}</div>
+  </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+  </div>
+`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity)
+
+
 
 
 
